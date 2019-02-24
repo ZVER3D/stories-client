63a76002d30f32eb5c233e520de0be4f1bfc8910
Ivan <ivanakazver3d@gmail.com>
2019-02-24 18:11:09 +0700

    Add very basic material ui layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
 
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  root: {
+    flexGrow: 1,
+  },
+});
+
 const Layout: React.FC = props => {
+  const classes = useStyles();
+
   return (
     <>
-      <div>Header</div>
+      <CssBaseline />
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Stories Site
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
       {props.children}
       <div>Footer</div>
     </>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
+import { install } from '@material-ui/styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+install();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
